9ac4d702dbbfada9c92cb911d72d3b6f2c726459
Tanel Põld <tanel@brightside.ee>
2024-01-08 12:19:54 +0200

    [TASK] remove ModifyPageEditButtons features

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
     'title' => 'BEUX',
     'description' => 'Back End UX thing',
     'category' => 'fe',
-    'version' => '0.1.2',
+    'version' => '0.2.0',
     'state' => 'stable',
     'clearCacheOnLoad' => 1,
     'author' => 'Tanel Põld',
